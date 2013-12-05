a6ff9b6808cbb7563d9ddcccd49996fbd0aea341
Dave Kerr <dwmkerr@gmail.com>
2013-12-05 14:39:34 +0100

    Update starfield.js
    Fixed height in constructor

--- starfield.js
+++ starfield.js
@@ -8,7 +8,7 @@ function Starfield() {
 	this.fps = 30;
 	this.canvas = null;
 	this.width = 0;
-	this.width = 0;
+	this.height = 0;
 	this.minVelocity = 15;
 	this.maxVelocity = 30;
 	this.stars = 100;
